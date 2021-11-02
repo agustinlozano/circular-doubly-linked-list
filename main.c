baa3dcab031882850fafef027361d38b125fba05
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-02 14:40:06 -0300

    Add main file

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "cdll.h"
+
+int main(void) {
+    Node *headCat = NULL;
+    Node *tailCat = NULL;
+
+    printf("Circular doubly linked list\n\n");
+    ejecutarMenu(headCat, tailCat);
+
+    return EXIT_SUCCESS;
+}
