a90b791cea3e3e063f95faccee8ad16031900e56
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-02 14:46:53 -0300

    Add header file

---
+++ cdll.h
@@ -0,0 +1,31 @@
+#ifndef CDLL
+#define CDLL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct objeto {
+    char content[30];
+    int ID;
+    struct objeto *next;
+    struct objeto *prev;
+} Obj;
+
+typedef struct node {
+    char content[30];
+    Obj *obj;
+    struct node *next;
+    struct node *prev;
+} Node;
+
+void runMenu(Node *headC, Node *tailC);
+
+void addNode(Node **head, Node **tail);
+void addObject(Node *headC, Node *tailC);
+void newObject(Node *currentCat);
+
+void printList(Node *head);
+void deleteNode(Node **head, Node **tail);
+
+#endif
