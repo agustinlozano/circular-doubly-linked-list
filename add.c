3dc78dafa552b6e157ec1f8393f260a629498849
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-03 15:33:18 -0300

    Refactoring to add functions

--- add.c
+++ add.c
@@ -6,80 +6,71 @@ void addNode(Node **head, Node **tail) {
     printf("\n\tIngresar la keyword del nodo: ");
     scanf(" %s", newNode -> content);
 
-    if (*head == NULL) {                                 //la lista esta vacia
+    //la lista esta vacia
+    if (*head == NULL) {
         printf("\n\tAgrega el primer nodo de la lista");
         (*head) = newNode;        
-        newNode -> next = (*head);
-        (*tail) = newNode;        
-        newNode -> prev = (*tail);
-    } else {                                             //la lista no esta vacia
-        printf("\n\tAgrega un nodo mas a la lista");    
-        (*tail) -> next = newNode;
-        newNode -> prev = (*tail);
         (*tail) = newNode;
-        newNode -> next = (*head);
-        (*head) -> prev = newNode;
+        doLinksAddCategory(head, tail, newNode);     
+    
+    //la lista no esta vacia
+    } else {
+        printf("\n\tAgrega un nodo mas a la lista");    
+        doLinksAddCategory(head, tail, newNode);
     }
 
-	printf("\n\tNodo ingresado\n\n");
-
+    printf("\n\tExito: categoria agregada\n\n");
 }
 
-void addObject(Node *headCat, Node *tailCat) {
-    Node *aux = headCat;
-    char category[30];
-
-    printf("\n\tIngrese la categoria que deseas buscar: ");
-    scanf(" %s", category);
-
-    int quit = 1;
-    do {
-        if(strcmp(aux->content, category) == 0){
-           printf("\n\tExito: categoria encontrada");
-
-           printf("\n\tAhora agregamos el nuevo objeto dentro de la categoria: %s\n",
-                aux -> content);
-           
-           newObject(aux);
-
-           quit = 0;
-        }
-
-        aux = aux -> next;
-    } while(quit);
-}
-
-void newObject(Node *currentCat) {
+void addObject(Node *headCat) {
     Obj *newObject = (Obj *)malloc(sizeof(Obj));
-    Obj *headObj = currentCat -> obj;
+    Obj *headObj = headCat -> obj;
     Obj *tailObj = NULL;
+
+    printf("\n\tSe agregara el nuevo objeto dentro de la categoria: %s\n",
+                headCat -> content);
 
     printf("\n\tIngresar la keyword del objeto: ");
     scanf(" %s", newObject -> content);
 
-    if (headObj == NULL) {                                 //la lista esta vacia
+    //la lista esta vacia
+    if (headObj == NULL) {
         printf("\n\tAgrega el primer objeto de la lista");
         newObject -> ID = 1;
 
-        headObj = newObject;        
-        newObject -> next = headObj;
-        tailObj = newObject;        
-        newObject -> prev = tailObj;
+        headObj = newObject;
+        tailObj = newObject;
 
-        currentCat -> obj = headObj;
-    } else {                                             //la lista no esta vacia
+        doLinksAddObjects(headObj, tailObj, newObject);
+
+        headCat -> obj = headObj;
+    
+    //la lista no esta vacia
+    } else {
         printf("\n\tAgrega un objeto mas a la lista");
         tailObj = headObj -> prev;
         int prevID = tailObj -> ID; 
 
-        tailObj -> next = newObject;
-        newObject -> prev = tailObj;
-        tailObj = newObject;
-        newObject -> next = headObj;
-        headObj -> prev = newObject;
+        doLinksAddObjects(headObj, tailObj, newObject);
 
         newObject -> ID = prevID + 1;
     }
 
     printf("\n\tExito: objeto agregado\n\n");
+}
+
+void doLinksAddCategory(Node **head, Node **tail, Node *newNode) {
+    (*tail) -> next = newNode;
+    newNode -> prev = (*tail);
+    (*tail) = newNode;
+    newNode -> next = (*head);
+    (*head) -> prev = newNode;
+}
+
+void doLinksAddObjects(Obj *headObj, Obj *tailObj, Obj *newObject) {
+    tailObj -> next = newObject;
+    newObject -> prev = tailObj;
+    tailObj = newObject;
+    newObject -> next = headObj;
+    headObj -> prev = newObject;    
 }
