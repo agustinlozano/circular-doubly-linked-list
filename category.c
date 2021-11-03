dedbeaa0f0702a89e97614efedacf7043ec16e2a
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-03 08:35:39 -0300

    Add new file: category functions

---
+++ category.c
@@ -0,0 +1,28 @@
+#include "cdll.h"
+
+void nextCategory(Node **head, Node **tail) {
+    Node *aux;
+    if (*head == NULL) {
+        printf("\nLa lista esta vacia\n");
+    } else {
+        aux = (*head) -> next;
+        (*tail) = (*head);
+        (*head) = aux;
+
+        printf("\nLa categoria actual es: %s\n\n", (*head)->content);        
+    }
+}
+
+//implementar esta ultima
+void prevCategory(Node **head, Node **tail) {
+    Node *aux;
+    if (*head == NULL) {
+        printf("\nLa lista esta vacia\n");
+    } else {
+        aux = (*head) -> prev;
+        (*tail) = (*tail) -> prev;
+        (*head) = aux;
+
+        printf("\nLa categoria actual es: %s\n\n", (*head)->content);        
+    }
+}
