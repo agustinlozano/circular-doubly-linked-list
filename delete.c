6d797dd0335294821169f22bd0cebbdb55dbf907
Agustin Lozano <agustinlozanoblua@gmail.com>
2021-11-03 15:22:16 -0300

    Refactoring to delete functions

--- delete.c
+++ delete.c
@@ -2,55 +2,26 @@
 
 void deleteNode(Node **head, Node **tail) {
     Node *current = *head;
-    Node *aux = NULL;
-
-    char category[30];
-
-    printf("\n\tIngrese la categoria que desea borrar: ");
-    scanf(" %s", category);
 
     if (current != NULL) {
-        do {
-            if (strcmp(current -> content, category) == 0) {
-                if (current == (*head) && current == (*tail)) {
-                    printf("\n\tSe elemininara el unico elemento existente");
-                    *head = NULL;
-                    *tail = NULL;
+        if (current == (*head) && current == (*tail)) {
+            printf("\n\tSe elemininara el unico elemento existente\n\n");
+            *head = NULL;
+            *tail = NULL;
+            free(current);
 
-                    free(current);
-                } else if (current == (*head)) {
-                    (*head) = (*head) -> next;
-                    (*head) -> prev = (*tail);
-                    (*tail) -> next = (*head);
+        } else if (current == (*head)) {
+            doLinksDeleteCategory(head, tail);
 
-                    free(current);
-                } else if (current == (*tail)) {
-                    aux = current -> prev;
-                    
-                    (*tail) = aux;
-                    (*tail) -> next = (*head);
-                    (*head) -> prev = (*tail);
-
-                    free(current);
-                } else {
-                    aux = current -> prev;
-                    aux -> next = current -> next;
-
-                    aux = current -> next;
-                    aux -> prev = current -> prev;
-
-                    free(current);
-                }
-                break;
-            }
-            current = current -> next;
-
-        } while (current != *head);
-        printf("\n\tEl nodo ha sido elininado\n\n");
+        } else if (current == (*tail)) {
+            doLinksDeleteCategory(head, tail);
+        }
         
     } else {
         printf("\n\tLa lista esta vacia\n\n");
     }
+
+    printf("\nSe ha eliminado el nodo seleccionado\n");
 }
 
 void deleteObject(Node *head) {
@@ -58,7 +29,6 @@ void deleteObject(Node *head) {
 
     if (head != NULL) {
         Obj *currentObj = head -> obj;
-        Obj *aux = NULL;
 
         printf("\nIngrese el ID del objeto ligado a %s: ", head->content);
         scanf(" %d", &ID);
@@ -67,34 +37,24 @@ void deleteObject(Node *head) {
             currentObj = currentObj -> next;
         }
 
-        printf("\nCurrent object es: %s\tID: %d\n\n", 
+        printf("\nEl object es: %s\tID: %d\n\n", 
             currentObj->content, currentObj->ID);
 
         if (currentObj == head -> obj) {
             //elimino el nodo y pero tambien tengo que apuntarlo desde la categoria
-            aux = currentObj -> prev;
-            aux -> next = currentObj -> next;
-
-            aux = currentObj -> next;
-            aux -> prev = currentObj -> prev;
-
-            head -> obj = aux;
+            head -> obj = doLinksDeleteObjects(currentObj);
 
             //Ahora actualizo los ID de los objetos
-            updateID(currentObj->prev, aux);
+            updateID(currentObj->prev, head->obj);
 
             free(currentObj);
 
         } else {
             //simplemente borro el nodo
-            aux = currentObj -> prev;
-            aux -> next = currentObj -> next;
-
-            aux = currentObj -> next;
-            aux -> prev = currentObj -> prev;
+            doLinksDeleteObjects(currentObj);
 
             //Ahora actualizo los ID de los objetos
-            updateID(currentObj->prev, aux);
+            updateID(currentObj->prev, head->obj);
 
             free(currentObj);
         }
@@ -102,6 +62,33 @@ void deleteObject(Node *head) {
     } else {
         printf("\n\tLa lista esta vacia\n\n");
     }
+}
+
+void doLinksDeleteCategory(Node **head, Node **tail) {
+    (*tail) = (*head) -> prev;
+    Node *aux = NULL;
+    
+    aux = (*tail);
+    aux -> next = (*head) -> next;
+
+    aux = (*head) -> next;
+    aux -> prev = (*head) -> prev;
+
+    free((*head));
+
+    (*head) = aux;
+}
+
+Obj * doLinksDeleteObjects(Obj *obj) {
+    Obj *aux = NULL;
+    
+    aux = obj -> prev;
+    aux -> next = obj -> next;
+
+    aux = obj -> next;
+    aux -> prev = obj -> prev;
+
+    return aux;
 }
 
 void updateID(Obj *tail, Obj *current) {
